0ed2b57f44b528f40b59d23df84b5d24404f6cab
wolfgang-302 <wolfgangmeiners@googlemail.com>
2022-06-21 20:55:02 +0200

    added some unittests

--- tests/test.py
+++ tests/test.py
@@ -20,12 +20,23 @@ class TestPoint_of_State(unittest.TestCase):
     def test_Air_calculation(self):
         Air = fluid_factory('Air')
         p = Air(T=273.15, P=1e5)
-        return self.assertEqual(p.H,CP.PropsSI('H','T',273.15,'P',1e5,'Air'))
+        ok = True
+        for arg in p.acceptable_args:
+            if not arg in ['T','P']:
+                ok = ok and (getattr(p,arg,None)==CP.PropsSI(arg,'T',273.15,'P',1e5,'Air'))
+        return self.assertEqual(ok,True)
 
     def test_Air_with_units_calculation(self):
         Air = fluid_factory('Air',with_units=True)
         p = Air(T=Q_(0,'degC'), P=Q_(1e5,'Pa'))
-        return self.assertEqual(p.H.m_as('J/kg'),CP.PropsSI('H','T',273.15,'P',1e5,'Air'))
+        ok = True
+        for arg in p.acceptable_args:
+            if not arg in ['T','P']:
+                ok = ok and (
+                    getattr(p,arg,None).bv == CP.PropsSI(arg,'T',273.15,'P',1e5,'Air')
+                )
+        return self.assertEqual(ok,True)
+        #return self.assertEqual(p._arg_list,['T',273.15,'P',1e5,'Air'])
 
     def test_HA_definition(self):
         HA = fluid_factory('HumidAir')
@@ -36,6 +47,28 @@ class TestPoint_of_State(unittest.TestCase):
         HA = fluid_factory('HumidAir', with_units=True)
         p = HA(T=Q_(293.15,'K'), W=10e-3)
         return self.assertEqual(p._arg_list,['T',293.15,'W',10e-3,'P',101325])
+    
+    def test_AH_calculation(self):
+        HA = fluid_factory('HumidAir')
+        p = HA(T=293.15,W=10e-3)
+        ok = True
+        for arg in p.acceptable_args:
+            if not p in ['T','W','P']:
+                ok = ok and (
+                    getattr(p,arg,None)==CP.HAPropsSI(arg,'T',293.15,'W',10e-3,'P',101325)
+                )
+        return self.assertEqual(ok,True)
+    
+    def test_AH_with_units_calculation(self):
+        HA = fluid_factory('HumidAir',with_units=True)
+        p = HA(T=Q_(293.15,'K'),W=10e-3)
+        ok = True
+        for arg in p.acceptable_args:
+            if not p in ['T','W','P']:
+                ok = ok and (
+                    getattr(p,arg,None).bv==CP.HAPropsSI(arg,'T',293.15,'W',10e-3,'P',101325)
+                )
+        return self.assertEqual(ok,True)
 
 if __name__ == '__main__':
 
