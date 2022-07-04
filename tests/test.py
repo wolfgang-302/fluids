0d50f0eac31abab8003e7728f05b1d48f8ea928a
Wolfgang Meiners <wolfgangmeiners@MBPvonWolfgang.fritz.box>
2022-07-04 15:04:34 +0200

    added some tests

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,28 @@ import sys
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from fluids import fluid_factory, Q_, Quantity
+from fluids.fluids import Point_of_State
+
 import CoolProp.CoolProp as CP
+
+class Test_Q_(unittest.TestCase):
+    
+    def test_bv(self):
+        return self.assertEqual(Q_(5,'N').bv == Q_(5,'N').base_value == 5, True)
+    
+    def test_percent(self):
+        return self.assertEqual(Q_(5,'percent').units, 'percent')
+    
+    def test_ppM(self):
+        return self.assertEqual(Q_(1000,'ppM').units, 'ppM')
+    
+    def test_percent_bv(self):
+        return self.assertEqual(Q_(5,'percent').bv, 0.05)
+    
+    def test_ppM_bv(self):
+        return self.assertEqual(Q_(1000,'ppM').bv, 0.001)
+    
+
 
 class Test_fluid_factory(unittest.TestCase):
     
@@ -49,6 +70,17 @@ class Test_fluid_factory(unittest.TestCase):
                     getattr(p,arg,None).bv == CP.PropsSI(arg,'T',273.15,'P',1e5,'Air')
                 )
         return self.assertEqual(ok,True)
+    
+    def test_Air_with_units_register(self):
+        Air = fluid_factory('Air',with_units=True)
+        Air.register('SMOLAR','J/mol/K')
+        return self.assertEqual('SMOLAR' in Air.acceptable_args,True)
+    
+    def test_Air_with_units_unregister(self):
+        Air = fluid_factory('Air',with_units=True)
+        Air.unregister('P')
+        return self.assertEqual('P' in Air.acceptable_args, False)
+        
 
     def test_HA_definition(self):
         HA = fluid_factory('HumidAir')
@@ -91,6 +123,16 @@ class Test_fluid_factory(unittest.TestCase):
                     getattr(p,arg,None).bv==CP.HAPropsSI(arg,'T',293.15,'W',10e-3,'P',101325)
                 )
         return self.assertEqual(ok,True)
+    
+    def test_HA_with_units_register(self):
+        HA = fluid_factory('HumidAir',with_units=True)
+        HA.register('Cha','J/kg/K')
+        return self.assertEqual('Cha' in HA.acceptable_args, True)
+    
+    def test_HA_with_units_unregister(self):
+        HA = fluid_factory('HumidAir',with_units=True)
+        HA.unregister('W')
+        return self.assertEqual('W' in HA.acceptable_args, False)
 
 if __name__ == '__main__':
 
